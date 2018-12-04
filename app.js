9dd1aac7b2570aa96a14328bcb764aa8d04e8168
Femi Adegoke <femiadegoke@MacBook-Pro.local>
2018-12-04 21:57:57 +0100

    Add build folder

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static('build'));
 
 // catch all request at index page
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build/index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // bind port
